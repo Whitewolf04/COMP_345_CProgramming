a62b33ad54dc7df7a29a145f275a9936e506f58f
Whitewolf04 <tominhtuan18@gmail.com>
2021-09-30 18:42:31 -0400

    First draft of headers file

--- Cards/Cards.h
+++ Cards/Cards.h
@@ -1,14 +1,30 @@
+#include <string>
 using namespace std;
 
-class Cards{
+class Cards
+{
+public:
+    string type;
+    Cards(string type){};
     void play();
-
 };
 
-class Deck{
-
+class Deck
+{
+private:
+    int size;
+public:
+    Deck(int size){};
+    int getDeckSize(){};
+    void draw();
 };
 
-class Hand{
-    
+class Hand
+{
+private:
+    int size;
+public:
+    Hand(int size){};
+    int getHandSize(){};
+    void draw();
 };
