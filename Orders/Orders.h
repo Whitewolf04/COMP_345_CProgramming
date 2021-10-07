ce8c805a3ae62889d4dde1ae327650b5f029c9fa
Omar <ogfficial@gmail.com>
2021-10-07 19:53:18 -0400

    updated main with latest Order classes

--- Orders/Orders.h
+++ Orders/Orders.h
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include <vector>
 #include <iostream>
 
@@ -103,4 +104,106 @@ struct Negotiate : public Order
     Negotiate();
     void validate();
     std::string execute();
+=======
+#include <vector>
+#include <iostream>
+
+struct Order
+{
+    private:
+    std::string type;  
+    friend std::ostream& operator<<(std::ostream &strm, const Order &order);
+
+    public:
+    Order();
+    Order(std::string newType);
+    void validate();
+    std::string execute();
+    std::string getType();
+    
+};
+
+struct OrdersList
+{
+    private:
+    std::vector<Order*> Order_List;
+    friend std::ostream& operator<<(std::ostream &strm, const OrdersList &olist);
+
+    public:
+    OrdersList();
+    OrdersList(const OrdersList &o);
+    OrdersList& operator =(const OrdersList &o);
+    void add(Order o);
+    Order getElement(int index);
+    void remove(int index);
+    void move();
+};
+
+
+
+struct Advance : public Order
+{
+    private:
+    friend std::ostream& operator<<(std::ostream &strm, const Advance &advance);
+    public:
+    Advance();
+    void validate();
+    std::string execute();
+};
+
+
+struct Deploy : public Order
+{
+    private:
+    friend std::ostream& operator<<(std::ostream &strm, const Deploy &deploy);
+    public:
+    Deploy();
+    void validate();
+    std::string execute();
+};
+
+
+struct Bomb : public Order
+{
+    private:
+    friend std::ostream& operator<<(std::ostream &strm, const Bomb &bomb);
+    public:
+    Bomb();
+    void validate();
+    std::string execute();
+};
+
+
+struct Blockade : public Order
+{
+    private:
+    friend std::ostream& operator<<(std::ostream &strm, const Blockade &blockade);
+
+    public:
+    Blockade();
+    void validate();
+    std::string execute();
+};
+
+struct Airlift : public Order
+{
+    private:
+    friend std::ostream& operator<<(std::ostream &strm, const Airlift &airlift);
+
+    public:
+    Airlift();
+    void validate();
+    std::string execute();
+};
+
+struct Negotiate : public Order
+{
+    private:
+    friend std::ostream& operator<<(std::ostream &strm, const Negotiate &negotiate);
+
+    public:
+    Negotiate();
+    void validate();
+    std::string execute();
+>>>>>>> 19bcf3a98dc6eeaf7a818c04f65e0d861c5d581e
 };
--- Orders/OrdersDriver.cpp
+++ Orders/OrdersDriver.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include "Orders.cpp"
 
 
@@ -63,4 +64,33 @@ int main(){
     std::cout << "Show copy of original list..." << "\n";
     std::cout << list_copy << "\n";
     
+=======
+#include "Orders.cpp"
+
+
+int main(){
+    
+    Advance order1;
+    Airlift order2;
+    OrdersList list;
+
+    //validation and execution of orders
+    order2.validate();
+    std::cout << order1.execute() << "\n";
+
+    list.add(order1);    
+    list.add(order2);
+
+    //iterate through the list
+    std::cout << list << "\n";
+
+    //remove first order
+    list.remove(0);
+
+    //move() not yet implemented
+
+    std::cout << list << "\n";
+    
+
+>>>>>>> 19bcf3a98dc6eeaf7a818c04f65e0d861c5d581e
 }
