5b7cad999b4405146744af5781638327268783bd
Whitewolf04 <tominhtuan18@gmail.com>
2021-10-10 23:28:53 -0400

    Updated Cards files

--- Cards/Cards.h
+++ Cards/Cards.h
@@ -1,46 +1,3 @@
-<<<<<<< HEAD
-#include <string>
-using namespace std;
-
-class Cards
-{
-private:
-    string types[5] = {"bomb", "reinforcement", "blockade", "airlift", "diplomacy"};
-public:
-    string type;
-    Cards(){};
-    Cards(string t){};
-    bool equals(Cards *other);
-    void play();
-};
-
-class Deck
-{
-private:
-    int size;
-    int deckIndex;
-    Cards *deck = new Cards[size];
-public:
-    Deck(int deckSize){};
-    int getDeckSize(){};
-    void add(Cards *newCard);
-    void remove(Cards *target);
-    void draw(Hand *hand, int handSize);
-};
-
-class Hand
-{
-private:
-    int size;
-    int handIndex;
-    Cards *hand = new Cards[size];
-public:
-    Hand(int handSize){};
-    int getHandSize(){};
-    void add(Cards *newCard);
-    void remove(Cards *target);
-};
-=======
 #include <string>
 #include <vector>
 using namespace std;
@@ -88,4 +45,3 @@ public:
     void drawCard(Deck *deck);
     void playCard(int index, Deck *deck);
 };
->>>>>>> CardsBranch
