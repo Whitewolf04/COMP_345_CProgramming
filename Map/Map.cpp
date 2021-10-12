209c4e1f0fce94ce220584154eb9c31d1780cdba
Farouk Ihdene <65618038+FI2000@users.noreply.github.com>
2021-10-12 10:54:10 -0400

    New map design
    Overhauled everything. Should be functional when not trying to break it on purpose

--- Map/Map.cpp
+++ Map/Map.cpp
@@ -17,13 +17,11 @@ string MapLoader::FileLoader(ifstream* stream)
 	}
 	else {
 		cout << "file doesn't exist\n";
-		cout << "Exiting for safety reasons...";
-		exit(0);
 	}
-
+	delete Mapname;
 	return string((std::istreambuf_iterator<char>(*fs)), std::istreambuf_iterator<char>());
 }
-void MapLoader::FileParser(string& str, vector<Continent>& c, vector<Territory>& t)
+bool MapLoader::FileParser(string& str, vector<Continent>& c, vector<Territory>& t)
 {
 	
 	//Making pointers to passed varaibles
@@ -117,7 +115,7 @@ void MapLoader::FileParser(string& str, vector<Continent>& c, vector<Territory>&
 			}
 
 			//Accessing the amount of armies that each continent has for its armies
-			int numOfArmies = C->at(IntegerNumberOfContinent -1).getContinentArmies();
+			int numOfArmies = C->at(IntegerNumberOfContinent -1).getCCB();
 
 			//Adding a Territory Object with the Above Values to the passed Vector of Territories
 			Territory t(TerritoryOrder, numOfArmies, *NameOfTerritory, IntegerNumberOfContinent);
@@ -166,13 +164,17 @@ void MapLoader::FileParser(string& str, vector<Continent>& c, vector<Territory>&
 		
 	
 	}
+
+	delete line;
 	if ((!ContinentMark || !TerritoryMark || !BorderMark) && valid) { //checks if all sections have been identified, if not cancels the map loading
 		cout << "Invalid file... Discarting Input Map\n";
 		cout << "Emptying Continent List\n";
 		C->clear();
 		cout << "Emptying Territory List\n";
 		T->clear();
+
 	};
+	return valid;
 	
 }
 
@@ -185,6 +187,9 @@ void MapLoader::addTerritory(Territory t, vector<Territory>* v) {
 }
 
 //Constructor//AssignmentOperator Definitions
+Territory::Territory() {
+
+};
 Territory::Territory(int i, int i2, string s, int i3) {
 	T_num = i;
 	NumOfArmies = i2;
@@ -196,6 +201,7 @@ Territory::Territory(const Territory& t) {
 	NumOfArmies = t.NumOfArmies;
 	T_name = t.T_name;
 	Continent_Number = t.Continent_Number;
+	NeighboringTerritories = t.NeighboringTerritories;
 }
 Territory &Territory::operator=(const Territory& t) {
 	return *this; }
@@ -203,16 +209,312 @@ Territory &Territory::operator=(const Territory& t) {
 Continent::Continent(int i, string s, int i2, string s2) {
 	C_num = i;
 	C_name = s;
-	NumOfArmies = i2;
+	CCBus = i2;
 	colour = s2;
 }
 Continent::Continent(const Continent& c) {
 	C_num = c.C_num;
 	C_name = c.C_name;
-	NumOfArmies = c.NumOfArmies;
+	CCBus = c.CCBus;
 	colour = c.colour;
 }
 Continent& Continent::operator=(const Continent& t) { return *this; }
+
+//Map Constructor
+Map::Map() {
+	
+	ifstream* stream = new ifstream();
+	string* MapInformation = new string();
+	vector<Continent>* Continents = new vector<Continent>;
+	vector<Territory>* Territories = new vector<Territory>;
+	bool works = false;
+	*MapInformation = MapLoader::FileLoader(stream);
+	works = MapLoader::FileParser(*MapInformation, *Continents, *Territories);
+	if (!works) {
+		cout << "The map is not valid, map object will be emptied.\n";
+	}
+	else {
+		for (int i = 0;i < Territories->size();i++) {
+			ListOfNodes->push_back(Territories->at(i));
+		}
+		for (int i = 0;i < Continents->size();i++) {
+			ListOfContinents->push_back(Continents->at(i));
+		}
+
+		for (int i = 0;i < ListOfNodes->size();i++) {
+			vector<int> x = Territories->at(i).getTerritoryConnexions();
+			for (int j = 0;j < x.size();j++) {
+				try {
+					ListOfNodes->at(i).addEdges(&ListOfNodes->at(x.at(j) - 1));
+
+				}
+				catch (exception e) {
+
+				}
+			}
+		}
+		validate();
+	}
+		delete Territories, Continents, stream, MapInformation;
+	
+}
+Map::Map(const Map& m) {
+	*ListOfNodes = *m.ListOfNodes;
+	*ListOfContinents = *m.ListOfContinents;
+}
+
+//Map methods
+void Map::printNodeList() {
+	for (int i = 0; i < ListOfNodes->size();i++){
+		cout << ListOfNodes->at(i) << endl;
+	}
+};
+void Map::printContinentList() {
+	for (int i = 0; i < ListOfContinents->size();i++) {
+		cout << "For Continent " << i << " : " << &ListOfContinents->at(i) << endl;
+		cout << ListOfContinents->at(i) << endl;
+		cout << "------------------" << endl;
+	}
+};
+bool Map::advance(int n, int m) {
+	//Used to see if a node is connected to another
+	bool possible = false;
+	for (int i = 0; i < ListOfNodes->at(n-1).getEdges().size();i++) {
+		if (ListOfNodes->at(n-1).getEdges().at(i) == &ListOfNodes->at(m-1)) {
+			possible = true;
+		}
+	}
+	if (!possible) {
+		//cout << "Movement from " << n << " to " << m << " is not possible" << endl;
+	}
+	return possible;
+}
+bool Map::PlayerCheck(int node, int playerN) {
+	//Used to check if a territory belongs to a certain playerN
+	return (ListOfNodes->at(node).getPlayerNumber() == playerN ? true : false);
+};
+bool Map::validate() {
+	bool TerritoriesConnected = false;
+	bool ContinentsConnected = false;
+	bool TerritoryhasOneContinent = false;
+	vector<int> destination;
+	
+	//For territory-country check
+	vector<string> names;
+	bool twice = false;
+	//Checks if a node is added two times to the list of nodes, this way, a node cannot have two continents AND there cannot be two same
+	//nodes in the same map
+	for (int i = 0;i < ListOfNodes->size();i++) {
+		if (find(names.begin(), names.end(), ListOfNodes->at(i).getTerritoryName()) == names.end()) {
+			names.push_back(ListOfNodes->at(i).getTerritoryName());
+		}
+		else {
+			cout << "An element exists twice.\n";
+			twice = true;
+			break;
+		}
+
+	}
+
+	if (!twice) { TerritoryhasOneContinent = true; }
+	if (TerritoryhasOneContinent) { cout << "Each territory belongs to one Continent\n"; }
+	else { cout << "a territory belongs to more than one Continent\n"; }
+	if (TerritoryhasOneContinent) {
+		//For Continent Subgraph Check
+		{
+			//This section will add to an array each node of each continent and try to find a permutation where it
+			//one node can advance to the next one and then try to do the same for the inverse of that permutation
+			//This way a continent will 100% be connected and be reachable from any node within it
+			cout << "Checking if the continents are connected subgraphs..." << endl;
+			for (int c = 1;c <= ListOfContinents->size();c++) {
+				cout << "CHECKING FOR CONTINENT NUMBER " << c << ". ---------------------------------------------\n";
+				vector<int> checkpoints;
+				vector<int> marks;
+				int starter = 0;
+				bool connectedForward = false;
+				bool connectedBackwards = false;
+				for (int j = 0;j < getNodesForContinent(c).size();j++) {
+					if (j == 0) {
+						starter = getNodesForContinent(c).at(j).getTerritoryNumber();
+					}
+					checkpoints.push_back(getNodesForContinent(c).at(j).getTerritoryNumber());
+				}
+
+				do {
+					marks = checkpoints;
+					if (starter == marks.at(0)) {
+
+						//Loop to check if the a path exists
+						for (int i = 0; i < marks.size() - 1;i++) {
+
+							if (!advance(marks.at(i), marks.at(i + 1))) {
+								connectedForward = false;
+								break;
+							}
+							else if (advance(marks.at(i), marks.at(i + 1)) && i == marks.size() - 2) {
+								cout << "Checking the following path: ";
+								for (int i = 0; i < marks.size();i++) {
+									cout << marks.at(i) << " ";
+								}
+								cout << endl;
+								cout << "Forward Path works for this continent\nNow checking if it works the other way around:";
+								connectedForward = true;
+							};
+
+						}
+						if (connectedForward) {
+							//Checking the backwards path
+							reverse(marks.begin(), marks.end());
+							for (int i = 0; i < marks.size();i++) {
+								cout << marks.at(i) << " ";
+							}
+							cout << endl;
+							for (int i = 0; i < marks.size() - 1;i++) {
+
+								if (!advance(marks.at(i), marks.at(i + 1))) {
+									break;
+								}
+								else if (advance(marks.at(i), marks.at(i + 1)) && i == marks.size() - 2) {
+									cout << "Path works for this continent\n";
+									connectedBackwards = true;
+								};
+							}
+							if (connectedBackwards) {
+								cout << "Also connected backwards, path is valid and continent.\n";
+							}
+
+						}
+					}
+					else {
+						break;
+					}//If They are connected, we break and go check the next continent
+					if (connectedForward && connectedBackwards) {
+						cout << "Continent Number " << c << " is connected\n";
+						ContinentsConnected = true;
+						break;
+					}
+					else {
+						ContinentsConnected = false;
+					}
+				} while (next_permutation(checkpoints.begin(), checkpoints.end()));
+				if (!ContinentsConnected) {
+					break;
+				}
+				cout << "FINISHED CHECKING---------------------------------------\n\n";
+			}
+		}
+		if (ContinentsConnected) { cout << "Continents are a connected subraph\n"; }
+		else { cout << "Continents are not a connected subraph\n"; }
+		if (ContinentsConnected) {
+
+			cout << "\n\n\nChecking if the entire graph is connected and accessible...\n";
+			vector<vector<int>> Cnodes;//Vector of a vector of nodes that contain all the nodes of a specific continent
+			for (int i = 0;i < ListOfContinents->size();i++) {
+				vector <int> add;
+				for (auto j : getNodesForContinent(i + 1)) {
+					add.push_back(j.getTerritoryNumber());
+				}
+				Cnodes.push_back(add);
+			}
+			bool exists = false;
+			//We check if every continent has a node that can be accessed from outside the continent
+			//We will later check if every continent leads to the outside
+			//Uses a vector of vector<int> called C(ontinent)nodes. If any node of this continent is a neighbor of a node
+			//that is isnt part of this continent, then it is possible to access this continent
+			for (int i = 0;i < Cnodes.size();i++) {
+				exists = false;
+				for (int j = 0;j < Cnodes.at(i).size();j++) {
+
+					for (int k = 0;k < ListOfNodes->size();k++) {
+
+						if (ListOfNodes->at(k).getTerritoryContinent() != i + 1) {
+							for (auto l : ListOfNodes->at(k).getTerritoryConnexions()) {
+								if (l == Cnodes.at(i).at(j)) {
+									destination.push_back(ListOfNodes->at(k).getTerritoryContinent() - 1);
+									cout << ListOfContinents->at(i).getContinentName() << " can be accessed from " << ListOfContinents->at(ListOfNodes->at(k).getTerritoryContinent() - 1).getContinentName() << endl;
+									exists = true;
+									break;
+								}
+							}
+						}
+						if (exists) { break; }
+					}
+				}
+				if (!exists) {
+					break;
+				}
+			}
+			if (!exists) {
+				cout << "A certain Continent cannot be accessed.\n";
+			}
+			else {
+				bool repeat = true;
+				//We check if every continent has a node that can be access a continent other than itself
+				cout << "\nEach continent can be accessed. Now checking if every continent leads to another one other than itself.\n\n";
+				bool canExit = false;
+				for (int i = 0;i < Cnodes.size();i++) {
+					canExit = false;
+					for (int j = 0;j < Cnodes.at(i).size();j++) {//This loop serves to see where each node connects to
+						
+						for (auto k : ListOfNodes->at(Cnodes.at(i).at(j) - 1).getTerritoryConnexions()) {
+							if (ListOfNodes->at(k - 1).getTerritoryContinent() - 1 != i) {// This is to check that the node does not connect to itself
+								cout << "From Continent " << ListOfContinents->at(i).getContinentName() << ", you can go to " << ListOfContinents->at(ListOfNodes->at(k - 1).getTerritoryContinent() - 1).getContinentName() << endl;
+								if (repeat && (getNodesForContinent(i+1).size()>2)) {
+									repeat = false;
+									continue;
+								}
+								else {
+									repeat = true;
+									canExit = true;
+									break;
+								}
+							}
+							if (canExit) {
+								break;
+							}
+						}
+						if (canExit) {
+							break;
+						}
+					}
+					if (!canExit) {
+						break;
+					}
+				}
+
+				if (!canExit) {
+					cout << "A territory acts as a trap.\n";
+				}
+				else {
+					TerritoriesConnected = true;
+				}
+			}
+			if (TerritoriesConnected) { cout << "Territories are connected\n"; }
+			else { cout << "Continents are not connected\n"; }
+
+		}
+
+
+		cout << endl << endl;
+	}
+
+	if (!(TerritoriesConnected && ContinentsConnected && TerritoryhasOneContinent)) {
+		cout << "The map is not valid.\n";
+		return false;
+	}
+	return true;
+}
+vector<Territory> Map::getNodesForContinent(int n) {
+	vector<Territory> v;
+	for (int i = 0;i < ListOfNodes->size();i++) {
+		if (ListOfNodes->at(i).getTerritoryContinent()==n){
+			v.push_back(ListOfNodes->at(i));
+		}
+	}
+
+	return v;
+	
+}
 
 //Method for identifying map file elements
 int Continent::Parsing(string line,string& element1, string& element2, string& element3) {
@@ -249,10 +551,12 @@ ostream& operator<<(ostream& os, const Continent& c)
 }
 ostream& operator<<(ostream& os, const Territory& t) {
 	string edges;
-	for (int i = 0; i < t.NeighboringTerritories->size();i++) {
-		edges = edges + to_string(t.NeighboringTerritories->at(i)) + " ";
+	for (int i = 0; i < t.NeighboringTerritories.size();i++) {
+		
+		edges = edges + to_string(t.NeighboringTerritories.at(i)) + " ";
 	}
-	os << "Territory Number : " << t.T_num << " , Territory Name : " << t.T_name << " , Territory armies # : " << t.NumOfArmies << " , Territoriy leads to : " << edges << endl;
+	os << "Territory Number : " << t.T_num << " , Territory Name : " << t.T_name <<
+		" , Territory armies # : " << t.NumOfArmies << " , Territoriy leads to : " << edges << endl;
 
 	return os;
 };
@@ -264,8 +568,8 @@ int Continent::getContinentNumber() {
 string Continent::getContinentName() {
 	return C_name;
 };
-int Continent::getContinentArmies() {
-	return NumOfArmies;
+int Continent::getCCB() {
+	return CCBus;
 };
 string Continent::getContinentColour() {
 	return colour;
@@ -284,23 +588,40 @@ int Territory::getTerritoryContinent() {
 int Territory::getPlayerNumber() {
 	return PlayerNumber;
 };
-vector<int>& Territory::getTerritoryConnexions() {
-	return *NeighboringTerritories;
+vector<int> Territory::getTerritoryConnexions() {
+	return NeighboringTerritories;
+}
+vector<Territory*>& Territory::getEdges() {
+	return Edges;
+};
+int Territory::getNumOfArmies() {
+	return NumOfArmies;
 }
 
-//Territory Mutators
+//Map Accessors
+Territory* Map::getNode(int n) {
+	return &ListOfNodes->at(n);
+};
+Continent* Map::getContinent(int n) {
+	return &ListOfContinents->at(n);
+};
+
+//Territory Game Related Methods
 void Territory::addNeighbors(int v) {
 	//For THIS territory, adding neighbors
-	NeighboringTerritories->push_back(v);
+	NeighboringTerritories.push_back(v);
 }
 void Territory::setNumArmies(int n) {
 	NumOfArmies = n; // Updates the number of armies
 };
 void Territory::setPlayerNumber(int n) {
-	PlayerNumber = n;
+	PlayerNumber = n; // Used whenever a Player gains control of a territory
+};
+void Territory::addEdges(Territory* p) {
+	Edges.push_back(p);
+};
+bool Territory::PlayerCheck(int n) {
+	return (PlayerNumber==n ? true : false); // Used whenever to check if a territory belongs to player N
 };
 
-bool Territory::PlayerCheck(int n) {
-	return (PlayerNumber==n ? true : false);
-};
 
--- Map/Map.h
+++ Map/Map.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 
@@ -11,7 +12,7 @@ class Continent {
 	private:
 		int C_num;
 		string C_name;
-		int NumOfArmies;
+		int CCBus;
 		string colour;
 	public:
 		//Constructors
@@ -22,8 +23,9 @@ class Continent {
 		//Accessors
 		int getContinentNumber();
 		string getContinentName() ;
-		int getContinentArmies();
+		int getCCB();
 		string getContinentColour();
+		
 
 		//Stream Insertion Operator
 		friend ostream& operator<<(ostream& os, const Continent& dt);
@@ -40,9 +42,12 @@ class Territory {
 		string T_name;
 		int Continent_Number;
 		int PlayerNumber;
-		vector<int>* NeighboringTerritories = new vector<int>;
+		vector<int> NeighboringTerritories;
+		vector<Territory*> Edges;
+
 	public:
 		//Constructors
+		Territory();
 		Territory(int num, int armies, string name, int Cont_num);
 		Territory(const Territory& t);
 		Territory& operator = (const Territory& t);// Not Working Properly
@@ -52,12 +57,15 @@ class Territory {
 		string getTerritoryName();
 		int getTerritoryContinent();
 		int getPlayerNumber();
-		vector<int>& getTerritoryConnexions();
+		int getNumOfArmies();
+		vector<int> getTerritoryConnexions();
+		vector<Territory*>& getEdges();
 
 		//Mutator
 		void addNeighbors(int n);
 		void setNumArmies(int n);
 		void setPlayerNumber(int n);
+		void addEdges(Territory* p);
 
 		//Checking if territory belongs to the passed player
 		bool PlayerCheck(int n);
@@ -69,15 +77,33 @@ class Territory {
 
 };
 
-struct Node {
 
-	
-};
+
+
 class Map {
 
-	public:	
 
+	public:	
+		//Constructors and destructor
+		Map();
+		Map(const Map& m);
+		Map(vector<Continent>& v, vector<Territory>& t);
+		//Display Methods
+		void printContinentList();
+		void printNodeList();
+		vector<Territory> getNodesForContinent(int n);
+		
+		//Game Related Methods
+		bool advance(int n, int m);
+		bool PlayerCheck(int node, int playerN);
+		bool validate();
+
+		//Accessors
+		Territory* getNode(int n);
+		Continent* getContinent(int n);
 	private:
+		vector<Territory>* ListOfNodes = new vector<Territory>;
+		vector<Continent>* ListOfContinents = new vector<Continent>;
 
 };
 
@@ -85,7 +111,7 @@ class MapLoader {
 
 	public:
 		static string FileLoader(ifstream* stream); // Checks if file exists
-		static void FileParser(string& str, vector<Continent>& v, vector<Territory>& c); // Add function to empty v and c if map is invalid
+		static bool FileParser(string& str, vector<Continent>& v, vector<Territory>& c); // Add function to empty v and c if map is invalid
 
 	private:
 		static void addContinents(Continent c, vector<Continent>* v);
@@ -93,3 +119,6 @@ class MapLoader {
 		vector<Continent>* Continents;
 		vector<Territory>* CountryList;
 };
+
+//Add dummy player class
+//validate method
